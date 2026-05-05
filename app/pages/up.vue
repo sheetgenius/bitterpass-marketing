27a64b11c871883768967898b5749b227798162c
Bitter Maintainer <maintainer@bitter.sh>
2026-05-05 10:31:27 +0000

    Serve BitterPass health as static page

---
+++ app/pages/up.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+useHead({
+  title: 'BitterPass health',
+  meta: [{ name: 'robots', content: 'noindex' }],
+})
+</script>
+
+<template>
+  <pre>{"ok":true,"service":"bitterpass.com"}</pre>
+</template>
